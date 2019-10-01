f8e1ba39aeaafcb58f02af834df62a30bf49ac77
Loay Abdelsalam <loay.abdelsalam@pin-offer.com>
2019-10-01 03:32:20 +0200

    SignIn and Signout Handlers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './stylesheet/main.scss';
-
+import { appConfig } from './utils/constants'
+import { UserSession } from 'blockstack'
 import { Button } from 'react-bulma-components'
-function App() {
+class App extends Component {
+  state = {
+    userSession : new UserSession({appConfig})
+  }
+
+  handleSignOut = ()=>{
+    console.log('Signout')
+  }
+
+  handleSignIn = ()=>{
+    console.log('Sign in')
+    const { userSession } = this.state
+    userSession.redirectToSignIn()
+  }
+render(){
+  const { userSession } = this.state
+
   return (
-    <div className="App">
-      <Button color="primary">
-        Hello Blockstack
-      </Button  >
-    </div>
-  );
+      <div className="App">
+      {
+        userSession.isUserSignedIn() ?
+        <Button color="primary" onClick={this.handleSignOut}>
+          Signout
+        </Button>
+        :
+        <Button color="primary" onClick={this.handleSignIn}>
+          Sign In
+        </Button>
+      }
+
+      </div>
+    )
+  }
 }
 
 export default App;
