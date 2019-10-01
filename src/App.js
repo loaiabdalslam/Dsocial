1cb791656fca1e82b34f706260a55a74f8817f06
Loay Abdelsalam <loay.abdelsalam@pin-offer.com>
2019-10-01 02:52:28 +0200

    add button and Bluma activat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './stylesheet/main.scss';
 
+import { Button } from 'react-bulma-components'
 function App() {
   return (
     <div className="App">
-    Hello Blockstack
+      <Button color="primary">
+        Hello Blockstack
+      </Button  >
     </div>
   );
 }
