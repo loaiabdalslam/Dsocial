81f0f663318abd1cd4f6a3d82553276260060ff0
loai <loaiabdalslam@gmail.com>
2019-10-05 15:21:11 +0200

    User Provider is Created

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,95 +1,95 @@
 import React, { Component } from 'react';
-import { Button,Card,Navbar } from 'react-bulma-components'
+import { Button, Card, Navbar } from 'react-bulma-components'
 import Login from './Login'
 import Logout from './Logout'
 import { appConfig } from '../utils/constants'
 import { UserSession } from 'blockstack'
 
 class NavbarTop extends Component {
-    state = {
-        userSession : new UserSession({appConfig})
-      }
-    
-      componentDidMount = async ()=>{
-        const { userSession } = this.state
-        if(!userSession.isUserSignedIn() && userSession.isSignInPending()){
-          const userData = await userSession.handlePendingSignIn()
-    
-          if(!userData.username){
-            throw new Error('This App Require a Username !')
-          }
-    
-          window.location = '/'
-    
-          console.log(userData)
-        }
+  state = {
+    userSession: new UserSession({ appConfig })
+  }
+
+  componentDidMount = async () => {
+    const { userSession } = this.state
+    if (!userSession.isUserSignedIn() && userSession.isSignInPending()) {
+      const userData = await userSession.handlePendingSignIn()
+
+      if (!userData.username) {
+        throw new Error('This App Require a Username !')
       }
 
+      window.location = '/'
+
+      console.log(userData)
+    }
+  }
 
 
-    render() {
-        const { userSession } = this.state
 
-        return (
-            <Navbar
-            color='info'
-            active={true}
-          >
-            <Navbar.Brand>
-              <Navbar.Item renderAs="a" href="#">
-                <img
-                  src="https://bulma.io/images/bulma-logo.png"
-                  alt="Bulma: a modern CSS framework based on Flexbox"
-                  width="112"
-                  height="28"
-                />
-              </Navbar.Item>
-              <Navbar.Burger/>
+  render() {
+    const { userSession } = this.state
 
-            </Navbar.Brand>
-            <Navbar.Menu>
-              <Navbar.Container>
-                <Navbar.Item>Second</Navbar.Item>
-              </Navbar.Container>
-              <Navbar.Container position="end">
+    return (
+      <Navbar
+        color='info'
+        active={true}
+      >
+        <Navbar.Brand>
+          <Navbar.Item renderAs="a" href="#">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              alt="Bulma: a modern CSS framework based on Flexbox"
+              width="112"
+              height="28"
+            />
+          </Navbar.Item>
+          <Navbar.Burger />
 
-              {
-                userSession.isUserSignedIn() ?
+        </Navbar.Brand>
+        <Navbar.Menu>
+          <Navbar.Container>
+            <Navbar.Item>Second</Navbar.Item>
+          </Navbar.Container>
+          <Navbar.Container position="end">
+
+            {
+              userSession.isUserSignedIn() ?
                 <React.Fragment>
-                <Navbar.Item>
-                Posts
+                  <Navbar.Item>
+                    Posts
                </Navbar.Item>
-               <Navbar.Item>
-               Profile
+                  <Navbar.Item>
+                    Profile
            </Navbar.Item>
 
-           
-               <Navbar.Item>
-               <Logout userSession={userSession} />
-               </Navbar.Item>
+
+                  <Navbar.Item>
+                    <Logout userSession={userSession} />
+                  </Navbar.Item>
 
                 </React.Fragment>
-:      
-<React.Fragment>
+                :
+                <React.Fragment>
 
-<Navbar.Item>
-                             
-<Login userSession={userSession} />
-</Navbar.Item>
+                  <Navbar.Item>
 
-</React.Fragment>
+                    <Login userSession={userSession} />
+                  </Navbar.Item>
+
+                </React.Fragment>
 
 
-                
-            
+
+
             }
-            </Navbar.Container>
-            </Navbar.Menu>
-          </Navbar>
-        )
+          </Navbar.Container>
+        </Navbar.Menu>
+      </Navbar>
+    )
 
-    }
+  }
 
-} 
+}
 
 export default NavbarTop;
---
+++ src/providers/UserProvider.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bulma-components'
+import PropType from 'prop-types'
+export const MyContext = React.createContext()
+
+class UserProvider extends Component {
+
+    constructor() {
+        super(props)
+
+
+        const userData = props.userSession.loadUserData()
+
+        this.state = {
+            currentUser: {
+                userData,
+                userSession: props.userSession,
+                username: userData.username
+            }
+        }
+
+    }
+
+    static propTypes = {
+        userSession: PropType.object.isRequired
+    }
+
+    render() {
+
+        return (
+            <MyContext.Provider value={{
+                state: this.state
+
+            }}>
+                {this.props.children}
+            </MyContext>
+
+        )
+    }
+
+}
+
+export default UserProvider
